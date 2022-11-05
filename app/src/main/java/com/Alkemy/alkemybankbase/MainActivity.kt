489c363931ebd9baafd787580e707445dd9d3327
Juan Alegre <jpalegree@gmail.com>
2022-11-05 17:08:49 -0300

    add core fragments and bottom navigation

--- app/src/main/java/com/Alkemy/alkemybankbase/MainActivity.kt
+++ app/src/main/java/com/Alkemy/alkemybankbase/MainActivity.kt
@@ -2,7 +2,11 @@ package com.Alkemy.alkemybankbase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.Alkemy.alkemybankbase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +18,44 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Initialize navHostFragment and navController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        //Method to handler the bottom Navigation visivility
+        observeDestinationChangeListener(navController)
+
+    }
+
+    private fun observeDestinationChangeListener(navController: NavController) {
+        navController.addOnDestinationChangedListener {controller, destination, arguments ->
+            when(destination.id){
+
+                R.id.splashFragment -> {
+                    binding.bottomNavigationView.visibility = View.GONE
+                }
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.visibility = View.GONE
+                }
+                R.id.homeFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.transactionsFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.topUpBalanceFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.paymentsLoadFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.sendMoneyFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+
+            }
+
+        }
     }
 }
--- app/src/main/java/com/Alkemy/alkemybankbase/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/Alkemy/alkemybankbase/ui/fragments/HomeFragment.kt
@@ -6,22 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.Alkemy.alkemybankbase.R
+import com.Alkemy.alkemybankbase.databinding.FragmentHomeBinding
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private lateinit var binding: FragmentHomeBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
     }
 
 
