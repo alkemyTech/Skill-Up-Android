3e341e3931e97086c822e4c4ec0aff3ab88c3f13
Alexacontra <2011alexca@gmail.com>
2022-11-13 20:48:51 -0300

    adapter update

--- app/src/main/java/com/Alkemy/alkemybankbase/utils/adapter/SendMoneyAdapter.kt
+++ app/src/main/java/com/Alkemy/alkemybankbase/utils/adapter/SendMoneyAdapter.kt
@@ -3,9 +3,24 @@ package com.Alkemy.alkemybankbase.utils.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.Alkemy.alkemybankbase.EnvioDinero
 import com.Alkemy.alkemybankbase.R
 import com.example.movie_showcase.ResponsesClass.Movies
 import com.example.recyclerviewexamplo.R
 
-class SendMoneyAdapter : RecyclerView.Adapter
+class SendMoneyAdapter ( private val envioDineroLIst:List<EnvioDinero>) : RecyclerView.Adapter<SendMoneyHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SendMoneyHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return SendMoneyHolderHolder(layoutInflater.inflate(R.layout.item_superbank, parent,false))
+    }
+
+    override fun onBindViewHolder(holder: SendMoneyHolder, position: Int) {
+        val item = superBankList[position]
+        holder.render(item)
+    }
+
+    override fun getItemCount(): Int = superBankList.size
+
 }
+
--- app/src/main/java/com/Alkemy/alkemybankbase/utils/adapter/SendMoneyHolder.kt
+++ app/src/main/java/com/Alkemy/alkemybankbase/utils/adapter/SendMoneyHolder.kt
@@ -1,4 +1,27 @@
 package com.Alkemy.alkemybankbase.utils.adapter
 
-class SendMoneyHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.Alkemy.alkemybankbase.EnvioDinero
+
+
+class SendMoneyHolder(view: View): RecyclerView.ViewHolder(view){
+
+    val EnvioDinero = view.findViewById<TextView>(R.id.tvSuperBankName)
+    val realName  = view.findViewById<TextView>(R.id.tvRealName)
+    val publisher = view.findViewById<TextView>(R.id.tvpublisher)
+    val photo = view.findViewById<ImageView>(R.id.tvSuperBank)
+
+    fun render (superBankModel :SuperBank){
+        SendMoneyAdapter.text = superBankModel.Superbank
+        realName.text = superBankModel.realName
+        publisher.text = superBankModel.publisher
+
+    }
 }
+
+
+
+
