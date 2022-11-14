a967eb75f57454a60c45b9ebd41ce06928b0c392
SanchezEmir <emir2104@gmail.com>
2022-11-13 21:11:35 -0500

    fix error v3

--- app/src/main/java/com/Alkemy/alkemybankbase/MenuMainHostActivity.kt
+++ app/src/main/java/com/Alkemy/alkemybankbase/MenuMainHostActivity.kt
@@ -1,14 +1,13 @@
 package com.Alkemy.alkemybankbase
 
-
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.Alkemy.alkemybankbase.databinding.ActivityMenuMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
-
+@AndroidEntryPoint
 class MenuMainHostActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMenuMainBinding
